50363b4af763a92aba0ffa97f8fd5d0516ea59c8
Kizune Xia <noswear316@gmail.com>
2023-07-18 13:04:18 +0300

    add terr less 5 task 4.2

--- terraform/less 5/res/variables.tf
+++ terraform/less 5/res/variables.tf
@@ -7,3 +7,16 @@ variable "ip" {
       error_message = "Must be form cidr IPv4"
     }
 }
+
+variable "ip_list" {
+    type = list(string)
+    description = "ip addresses"
+    default = [ "192.168.0.1", "1.1.1.1", "1270.0.0.1" ]
+    validation {
+          condition = alltrue([
+      for a in var.ip_list : can(regex("^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", a ))
+    ])
+    error_message = "Must be form cidr IPv4"
+    }
+  
+}
